c28363a472e305912a7aa0b57ce19515b4b5c3a2
Garretlil <ed763135@gmail.com>
2024-07-28 09:15:08 +0300

    Auth,Registration,getting catalog,making order

---
+++ main/server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"database/sql"
+	_ "encoding/json"
+	"fmt"
+	_ "github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	_ "html/template"
+	"log"
+	"net/http"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "door"
+	dbname   = "testServer"
+)
+
+var database *sql.DB
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Println(err)
+	}
+
+	database = db
+	defer db.Close()
+
+	http.HandleFunc("/", getCatalog)
+	http.HandleFunc("/orders", newOrder)
+	http.HandleFunc("/auth", Authentication)
+	http.HandleFunc("/register", Registration)
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "About Page")
+	})
+
+	//start server
+	fmt.Println("Server is listening...")
+	http.ListenAndServe(":8181", nil)
+}
